5a080a0c1d10430ffb525901d13e35d4a851b747
Luca Tagliani <l.tagliani@cbt.it>
2013-07-18 14:20:33 +0200

    Implemented the plugin logic.

--- src/main/java/com/github/lucapino/manifest/ManifestMojo.java
+++ src/main/java/com/github/lucapino/manifest/ManifestMojo.java
@@ -15,63 +15,47 @@
  */
 package com.github.lucapino.manifest;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
+import java.util.Enumeration;
 import java.util.List;
-import org.apache.maven.model.Dependency;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
+import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
-import org.sonatype.aether.artifact.Artifact;
-import org.sonatype.aether.resolution.VersionRangeRequest;
-import org.sonatype.aether.resolution.VersionRangeResult;
-import org.sonatype.aether.util.artifact.DefaultArtifact;
-import org.sonatype.aether.version.Version;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.StringUtils;
 
 /**
  * Goal which generate a version list.
  *
- * @goal list
+ * @goal update
  *
  */
 public class ManifestMojo extends AbstractMojo {
+
     /**
-     * Starting version
+     * Subset of comma separated artifactIds to modify
+     *
+     * @parameter
+     */
+    private String includeArtifactIds;
+    /**
+     * Destination directory
      *
      * @parameter
      * @required
      */
-    private String startingVersion;
-    /**
-     * Starting version
-     *
-     * @parameter default-value="false"
-     * @required
-     */
-    private boolean includeSnapshots;
-    /**
-     * GroupId of project.
-     *
-     * @parameter default-value="${project.groupId}
-     * @required
-     */
-    private String groupId;
-    /**
-     * ArtifactId of project.
-     *
-     * @parameter default-value="${project.artifactId}
-     * @required
-     */
-    private String artifactId;
-    /**
-     * Name of the property that contains the ordered list of versions
-     * requested.
-     *
-     * @parameter default-value="${project.artifactId}
-     * @required
-     */
-    private String versionListPropertyName;
+    private File destDir;
+
     /**
      * The Maven project
      *
@@ -83,9 +67,52 @@ public class ManifestMojo extends AbstractMojo {
     @Override
     public void execute() throws MojoExecutionException {
         try {
-            List<Dependency> dependencies = project.getDependencies();
-            for (Dependency dependency : dependencies) {
-                dependency.
+            // creates an arraylist of string with included artifcatIds
+            List<String> artifactIds = new ArrayList<String>();
+            if (StringUtils.isNotEmpty(includeArtifactIds)) {
+                String[] artifactIdArray = includeArtifactIds.split(",");
+                for (String artifactId : artifactIdArray) {
+                    artifactIds.add(artifactId);
+                }
+            }
+            Set<Artifact> artifacts = project.getDependencyArtifacts();
+            for (Artifact artifact : artifacts) {
+                if (artifactIds.contains(artifact.getArtifactId())) {
+                    JarFile jarFile = new JarFile(artifact.getFile());
+                    Manifest manifest = jarFile.getManifest();
+                    // write it to temp library
+                    File tempFile = File.createTempFile("MANIFEST", null);
+                    manifest.write(new FileOutputStream(tempFile));
+                    String content = FileUtils.fileRead(tempFile, "UTF-8");
+                    FileUtils.fileWrite(tempFile, "Trusted-Library: true\n" + content);
+
+                    String fileName = jarFile.getName();
+                    String fileNameLastPart = fileName.substring(fileName.lastIndexOf(File.separator));
+                    File destFile = new File(destDir, fileNameLastPart);
+
+                    JarOutputStream jos = new JarOutputStream(new FileOutputStream(destFile), new Manifest(new FileInputStream(tempFile)));
+                    Enumeration<JarEntry> entries = jarFile.entries();
+
+                    while (entries.hasMoreElements()) {
+                        JarEntry entry = entries.nextElement();
+                        if (!entry.getName().toLowerCase().endsWith("manifest.mf")) {
+                            InputStream is = jarFile.getInputStream(entry);
+
+                            //jos.putNextEntry(entry);
+                            //create a new entry to avoid ZipException: invalid entry compressed size
+                            jos.putNextEntry(new JarEntry(entry.getName()));
+                            byte[] buffer = new byte[4096];
+                            int bytesRead = 0;
+                            while ((bytesRead = is.read(buffer)) != -1) {
+                                jos.write(buffer, 0, bytesRead);
+                            }
+                            is.close();
+                            jos.flush();
+                            jos.closeEntry();
+                        }
+                    }
+                    jos.close();
+                }
             }
         } catch (Exception ex) {
             throw new MojoExecutionException("Error in plugin", ex.getCause());
